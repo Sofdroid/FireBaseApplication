f5bb4abdb3f702e0bd6ba295455bb323b12f507e
sofien <azouzisofien123@gmail.com>
2016-11-10 22:02:51 +0100

    Create a new createAccount which takes in an email address and password, validates them and then creates a new user with the createUserWithEmailAndPassword method.

--- app/src/main/java/firebase/sofdroid/com/firebaseapplication/AuthenticationActivity.java
+++ app/src/main/java/firebase/sofdroid/com/firebaseapplication/AuthenticationActivity.java
@@ -16,13 +16,63 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
-
+    private EditText inputEmail, inputPassword;
+    private Button  btnResetPassword;
+    private FirebaseAuth firebaseAuth;
+    String email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
 
+        //Get Firebase firebaseAuth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnResetPassword = (Button) findViewById(R.id.email_sign_in_button);
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = inputEmail.getText().toString().trim();
+                password = inputPassword.getText().toString().trim();
+
+                // Test Email
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Test Password
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Test short Password
+                if (password.length() < 5) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 5 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //Create user
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(AuthenticationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(AuthenticationActivity.this, "createUserWithEmail:onComplete: " + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(AuthenticationActivity.this, "Authentication failed. " + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(AuthenticationActivity.this, "Authentication Success. " + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
     @Override
     protected void onResume() {
